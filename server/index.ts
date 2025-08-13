8ddc88d699f08565f2357522ef01d3dfaaf0a3f5
Azat S. <to@azat.io>
2025-08-13 22:52:31 +0300

    chore: update dependencies

--- server/index.ts
+++ server/index.ts
@@ -7,96 +7,101 @@ interface OpenGraphPluginOptions {
   host: string
 }
 
-export let openGraphPlugin =
-  ({
-    twitterCard = 'summary',
-    defaultImage,
-    twitterSite,
-    host,
-  }: OpenGraphPluginOptions): Plugin =>
-  () => ({
-    extendsPage: page => {
-      let head = page.frontmatter.head ?? []
+export function openGraphPlugin({
+  twitterCard = 'summary',
+  defaultImage,
+  twitterSite,
+  host,
+}: OpenGraphPluginOptions): Plugin {
+  return function () {
+    return {
+      extendsPage: page => {
+        let head = page.frontmatter.head ?? []
 
-      head.push(['meta', { property: 'og:locale', content: page.lang }])
+        head.push(['meta', { property: 'og:locale', content: page.lang }])
 
-      if (page.title) {
-        head.push(['meta', { property: 'og:title', content: page.title }])
-      }
+        if (page.title) {
+          head.push(['meta', { property: 'og:title', content: page.title }])
+        }
 
-      if (page.frontmatter.description) {
-        head.push([
-          'meta',
-          { content: page.frontmatter.description, property: 'og:description' },
-        ])
-      }
-
-      if (!page.frontmatter.layout) {
-        head.push(['meta', { property: 'og:type', content: 'article' }])
-        if (page.frontmatter.date) {
+        if (page.frontmatter.description) {
           head.push([
             'meta',
             {
-              content: new Date(page.frontmatter.date).toISOString(),
-              property: 'article:published_time',
+              content: page.frontmatter.description,
+              property: 'og:description',
             },
           ])
         }
-      }
 
-      head.push([
-        'meta',
-        { content: `${host}${page.path}`, property: 'og:url' },
-      ])
+        if (!page.frontmatter.layout) {
+          head.push(['meta', { property: 'og:type', content: 'article' }])
+          if (page.frontmatter.date) {
+            head.push([
+              'meta',
+              {
+                content: new Date(page.frontmatter.date).toISOString(),
+                property: 'article:published_time',
+              },
+            ])
+          }
+        }
 
-      if (page.frontmatter['image']) {
         head.push([
           'meta',
-          {
-            content: page.frontmatter['image'] as string,
-            property: 'og:image',
-          },
+          { content: `${host}${page.path}`, property: 'og:url' },
         ])
-      } else if (defaultImage) {
-        head.push(['meta', { content: defaultImage, property: 'og:image' }])
-      }
 
-      if (page.title) {
-        head.push(['meta', { name: 'twitter:title', content: page.title }])
-      }
+        if (page.frontmatter['image']) {
+          head.push([
+            'meta',
+            {
+              content: page.frontmatter['image'] as string,
+              property: 'og:image',
+            },
+          ])
+        } else if (defaultImage) {
+          head.push(['meta', { content: defaultImage, property: 'og:image' }])
+        }
 
-      if (page.frontmatter.description) {
-        head.push([
-          'meta',
-          {
-            content: page.frontmatter.description,
-            name: 'twitter:description',
-          },
-        ])
-      }
+        if (page.title) {
+          head.push(['meta', { name: 'twitter:title', content: page.title }])
+        }
 
-      if (page.frontmatter['image']) {
-        head.push([
-          'meta',
-          {
-            content: page.frontmatter['image'] as string,
-            name: 'twitter:image',
-          },
-        ])
-      } else {
-        head.push([
-          'meta',
-          { content: '/hero-preview.png', name: 'twitter:image' },
-        ])
-      }
+        if (page.frontmatter.description) {
+          head.push([
+            'meta',
+            {
+              content: page.frontmatter.description,
+              name: 'twitter:description',
+            },
+          ])
+        }
 
-      head.push(['meta', { name: 'twitter:card', content: twitterCard }])
+        if (page.frontmatter['image']) {
+          head.push([
+            'meta',
+            {
+              content: page.frontmatter['image'] as string,
+              name: 'twitter:image',
+            },
+          ])
+        } else {
+          head.push([
+            'meta',
+            { content: '/hero-preview.png', name: 'twitter:image' },
+          ])
+        }
 
-      if (twitterSite) {
-        head.push(['meta', { name: 'twitter:site', content: twitterSite }])
-      }
+        head.push(['meta', { name: 'twitter:card', content: twitterCard }])
 
-      page.frontmatter.head = head
-    },
-    name: 'vuepress-plugin-open-graph',
-  })
+        if (twitterSite) {
+          head.push(['meta', { name: 'twitter:site', content: twitterSite }])
+        }
+
+        page.frontmatter.head = head
+      },
+      name: 'vuepress-plugin-open-graph',
+    }
+  }
+}
